b91863b310b475fe4fa3c688f6b4a81b769dbf99
Nikolai Bjørndalseter <nikolai.bjorndalseter@intility.no>
2022-02-17 10:48:10 +0100

    Added AutoMapper AssertConfigurationIsValid call in Program.cs

--- HotChocolateAutoMapperProjectionIssue/Program.cs
+++ HotChocolateAutoMapperProjectionIssue/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
 
+mapperConfig.AssertConfigurationIsValid();
+
 IMapper mapper = mapperConfig.CreateMapper();
 
 builder.Services.AddGraphQLServer()
